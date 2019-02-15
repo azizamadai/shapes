c741bf0b34cafcde47c382bfad2ba732e81655c6
csPython <cspython@AD25TF04NGF1J-96.local>
2019-02-15 09:55:56 -0500

    added more shapes

--- shapes5AA.py
+++ shapes5AA.py
@@ -1,11 +1,23 @@
 from graphics import*
 
-bTriWin = GraphWin("Blue Triangle", 300, 300)
-bTriWin.setCoords(0, 0, 300, 300)
+bTriWin = GraphWin("Blue Triangle", 600, 600)
+bTriWin.setCoords(0, 0, 600, 600)
 
-bTri = Polygon (Point(100, 100), Point(150, 200), Point(200,100))
-bTri.setFill(color_rgb(30, 30, 230))
+bTri = Polygon (Point(30, 30), Point(90, 90), Point(210,30))
+bTri.setFill(color_rgb(132, 112, 255))
 bTri.draw(bTriWin)
+
+bTri = Polygon (Point(570, 530), Point(530, 510), Point(510,570), Point(540, 540))
+bTri.setFill(color_rgb(64, 224, 208))
+bTri.draw(bTriWin)
+
+bTri = Polygon (Point(30, 570), Point(30, 500), Point(110, 500), Point(110, 570))
+bTri.setFill(color_rgb(250, 128, 114))
+bTri.draw(bTriWin)
+
+##bTri = Polygon (Point(570, 570), Point(30, 500), Point(110, 500), Point(110, 570))
+##bTri.setFill(color_rgb(250, 128, 114))
+##bTri.draw(bTriWin)
 
 bTriWin.getMouse()
 bTriWin.close()
