e8f1cd6f4666b64803b047ffee76586fd03133b6
csPython <cspython@AD25TF04NGF1J-96.local>
2019-03-05 11:45:15 -0500

    added the blue diamond in the center of the window

--- shapes5AA.py
+++ shapes5AA.py
@@ -1,6 +1,6 @@
 from graphics import*
 
-bTriWin = GraphWin("Blue Triangle", 600, 600)
+bTriWin = GraphWin("Blue Triangle!", 600, 600)
 bTriWin.setCoords(0, 0, 600, 600)
 
 bTri = Polygon (Point(30, 30), Point(90, 90), Point(210,30))
@@ -15,9 +15,13 @@ bTri = Polygon (Point(30, 570), Point(30, 500), Point(110, 500), Point(110, 570)
 bTri.setFill(color_rgb(250, 128, 114))
 bTri.draw(bTriWin)
 
-##bTri = Polygon (Point(570, 570), Point(30, 500), Point(110, 500), Point(110, 570))
-##bTri.setFill(color_rgb(250, 128, 114))
-##bTri.draw(bTriWin)
+bTri = Polygon (Point(570, 30), Point(530, 90), Point(510, 80), Point(510, 60))
+bTri.setFill(color_rgb(255, 20, 147))
+bTri.draw(bTriWin)
+
+bTri = Polygon (Point(200, 300), Point(300, 400), Point(400, 300), Point(300, 200))
+bTri.setFill(color_rgb(100, 149, 237))
+bTri.draw(bTriWin)
 
 bTriWin.getMouse()
 bTriWin.close()
